fb8c484e98f6dd37401a4ac92c029665680d56cf
Vladimir <vi142696@gmail.com>
2021-09-30 23:05:31 +0300

    add: postgresql

--- TestSite/TestSite/settings.py
+++ TestSite/TestSite/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'TestSite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER' : 'vovinsa',
+        'PASSWORD' : 'qwerty142696',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
