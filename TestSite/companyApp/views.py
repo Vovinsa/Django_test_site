c1b0cc72de13db5f94bd27e3ff66f8d388e37fb6
Vladimir <vi142696@gmail.com>
2021-10-07 17:47:42 +0300

    add: viewsets

--- TestSite/companyApp/views.py
+++ TestSite/companyApp/views.py
@@ -1,24 +1,27 @@
+from rest_framework.viewsets import GenericViewSet
+
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
+from django_filters import rest_framework as filters
+
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import CompanySerializer, EmployeeSerializer, CompanyListSerializer, EmployeeListSerializer
 
 from .models import Company, Employee
 
-from rest_framework.viewsets import GenericViewSet
-
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from .core.mixins import SerializerMixin
-
-from rest_framework.filters import OrderingFilter
-
-from rest_framework.permissions import IsAuthenticated
 
 class CompanyView(SerializerMixin, ListModelMixin, CreateModelMixin, GenericViewSet):
     serializers = {'list': CompanyListSerializer, 'create': CompanySerializer}
     queryset = Company.objects.all()
-    filter_backends = (OrderingFilter,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('companies')
     permission_classes = (IsAuthenticated,)
 
 class EmployeeView(SerializerMixin, ListModelMixin, CreateModelMixin ,GenericViewSet):
     serializers = {'list': EmployeeListSerializer, 'create': EmployeeSerializer}
     queryset = Employee.objects.all()
-    filter_backends = (OrderingFilter,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('name')
     permission_classes = (IsAuthenticated,)
