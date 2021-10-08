e1b70d6ffb6a19877b7a4774b75a3b989d45aeff
Vladimir <vi142696@gmail.com>
2021-10-08 15:59:18 +0300

    add: swagger

--- TestSite/companyApp/urls.py
+++ TestSite/companyApp/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+
 from django.urls import path
+
 from django.urls.conf import include
+
 from .views import *
+
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('company/create/', CompanyView.as_view({'post': 'create'}), name='company_create'),
     path('employee/create/', EmployeeView.as_view({'post': 'create'}), name='employee_create'),
     path('company/list/', CompanyView.as_view({'get': 'list'}), name='company'),
     path('employee/list/', EmployeeView.as_view({'get': 'list'}), name='employee'),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
 ]
+
+urlpatterns += doc_urls
